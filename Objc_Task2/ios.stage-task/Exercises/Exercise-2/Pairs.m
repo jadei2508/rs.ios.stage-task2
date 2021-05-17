23eb82b9cf384126462bc741449543ce65219ffc
Roman <68894436+jadei2508@users.noreply.github.com>
2021-05-17 23:49:43 +0300

    fix: update method countPairs

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+   NSInteger count = 0;
+
+        if (array.count != 0) {
+            for (NSNumber *item in array) {
+                for (NSNumber *item2 in array) {
+                    if (item.intValue - item2.intValue == number.intValue) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        return count;
 }
 
 @end
